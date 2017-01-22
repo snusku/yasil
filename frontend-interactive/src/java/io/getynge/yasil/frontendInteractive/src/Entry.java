3236a1667de0b163579adefd437900ead2aa5f9d
Michael Sullivan <icecreamman1917@gmail.com>
2017-01-22 18:34:22 -0500

    Initial Commit

---
+++ frontend-interactive/src/java/io/getynge/yasil/frontendInteractive/src/Entry.java
@@ -0,0 +1,7 @@
+package io.getynge.yasil.frontendInteractive.src;
+
+/**
+ * Created by spark on 1/22/2017.
+ */
+public class Entry {
+}
---
+++ frontend-interactive/test/java/io/getynge/yasil/frontedInteractive/src/BasicTest.java
@@ -0,0 +1,7 @@
+package io.getynge.yasil.frontedInteractive.src;
+
+/**
+ * Created by spark on 1/22/2017.
+ */
+public class BasicTest {
+}

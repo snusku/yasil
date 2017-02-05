49e35e55ea11578ba2f3f56baa7b6569300bd1a0
snusku <hyphpeng@gmail.com>
2017-02-05 16:24:57 -0500

    removed IDType

--- backend/src/java/io/getynge/yasil/backend/src/AExecutor.java
+++ backend/src/java/io/getynge/yasil/backend/src/AExecutor.java
@@ -8,22 +8,21 @@ package io.getynge.yasil.backend.src;
  * The location memory, call stack, and main stack are all managed by an accompanying IStateManager
  * The storage of source code, and potential translation is handled by AParser
  * The implementation of AExecutor should not have any support for generics
- * @param <LocationType> data type of locations, this data type should match the type received by the parser
+ * @param <LocationType> data type of locations, this data type should match the type sent and received by the parser
  * @param <NameType> data type of function names, this data type should match the type received by the parser
  * @param <OutputType> data type of output, this should match the type received by the bridge
  * @param <SourceType> data type of source code, this should match the type sent by the parser
- * @param <IDType> data type of IDs, this should match the type sent by the parser
  * @param <ResultType> data type of results (not output), this should match the type sent to IStateManager
  * @see ASenderReceiverPair
  * @see AParser
  * @see IStateManager
  */
-abstract class AExecutor<LocationType, NameType, OutputType, SourceType, IDType, ResultType> {
+abstract class AExecutor<LocationType, NameType, OutputType, SourceType, ResultType> {
     ISender<LocationType> sendLocationToParser;
     ISender<NameType> sendNameToParser;
     ISender<OutputType> sendOutputToBridge;
     IReceiver<SourceType> getSourceFromParser;
-    IReceiver<IDType> getIDFromParser;
+    IReceiver<LocationType> getLocationFromParser;
     IStateManager<LocationType, ResultType> stateManager;
 
     abstract void start();
--- backend/src/java/io/getynge/yasil/backend/src/AParser.java
+++ backend/src/java/io/getynge/yasil/backend/src/AParser.java
@@ -6,16 +6,15 @@ package io.getynge.yasil.backend.src;
  * This class will receive source code from the bridge, in realtime if interactive or all at once if not.
  * If the program is not in interactive mode, this class should be the only one on the thread storing any source code
  * @param <SourceType> The type of source code, should match the type received by AExecutor
- * @param <IDType> The type of IDs, should match the type received by AExecutor
- * @param <LocationType> The type of locations, should match the type sent by AExecutor
+ * @param <LocationType> The type of locations, should match the type sent and received by AExecutor
  * @param <NameType> The type of names, should match the type sent by AExecutor
  * @param <RawSourceType> The type of the source sent by ABridge, may match SourceType (but doesn't have to)
  * @see AExecutor
  * @see ASenderReceiverPair
  */
-abstract class AParser<SourceType, IDType, LocationType, NameType, RawSourceType> {
+abstract class AParser<SourceType, LocationType, NameType, RawSourceType> {
     ISender<SourceType> sendSourceToExecutor;
-    ISender<IDType> sendIDToExecutor;
+    ISender<LocationType> sendLocationToExecutor;
     IReceiver<LocationType> getLocationFromExecutor;
     IReceiver<NameType> getNameFromExecutor;
     IReceiver<RawSourceType> getSourceFromBridge;
